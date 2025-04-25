a2b4f5eec28ea697433e87fdea27b64cfd9250c0
Brent <r0888481@student.thomasmore.be>
2025-04-25 13:41:36 +0200

    combell setup

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use App\Http\Middleware\ActiveUser;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 // Routes
 Route::view('/', 'home')->name('home');
 Route::view('about','about')->name('about');
 Route::view('project', 'projects')->name('projects');
+
+Route::get('/symlink', function () {
+    Artisan::call('storage:link');
+});
 
 // checking if the user is authenticated, verified and active
 Route::middleware([
